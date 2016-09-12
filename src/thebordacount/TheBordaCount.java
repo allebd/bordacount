60c03f9ef2189e65a826aebd3a18c98791e6fddd
Bella Oyedele <delebella@yahoo.com>
2016-09-12 17:00:02 +0100

    Add files via upload

---
+++ src/thebordacount/TheBordaCount.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package thebordacount;
+
+/**
+ *
+ * @author Alleb_D
+ */
+public class TheBordaCount {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Login thelogin = new Login();
+        thelogin.setVisible(true);
+    }
+    
+}
